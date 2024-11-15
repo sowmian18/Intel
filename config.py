f40c5414ef5ba316e822ca94f89503c661bed8af
sowmian18 <sowmian18@gmail.com>
2024-11-15 23:16:33 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY VIPMODXOWNER
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7691002351:AAGeVHN12qIADKumYCuIPDigCgA1s8nwS2g")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@Sowmi_2005")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1307893826")
